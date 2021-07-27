6a0d53db2ee1509149a19218300d927a9cc7b0d8
YoonSung Kim <opqudk@gmail.com>
2021-07-27 14:28:51 +0900

    type setting

---
+++ signup/src/types/address.ts
@@ -0,0 +1,12 @@
+export type Address = {
+  zip: string;
+  address1: string;
+  address2: string | '';
+};
+
+export type DaumAdddress = {
+  address: string;
+  autoJibunAddress: string;
+  roadAddress: string;
+  sigunguCode: string;
+};
